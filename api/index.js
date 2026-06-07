7ed4fed13a6b753c255d5b8ba3e3ee6f31ae97e6
Ramonprojects <adm@ramonmago.com>
2026-06-06 22:03:00 -0300

    chore: shareCode DPVI48GV2WQ -> H97WJAQ3L06 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/vqjid4fa?shareCode=DPVI48GV2WQ&afp3=tap100&home=1" });
+    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/vqjid4fa?shareCode=H97WJAQ3L06&afp3=tap100&home=1" });
     res.end();
   }
