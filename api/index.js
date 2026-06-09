fed005262e86b2e6fee6f12090d75d585af871a2
Ramonprojects <adm@ramonmago.com>
2026-06-09 13:38:38 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/vqjid4fa?shareCode=CPFN75EWNI5&afp3=tap100&home=1" });
+    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=26758?shareCode=CPFN75EWNI5&afp3=tap100&home=1" });
     res.end();
   }
